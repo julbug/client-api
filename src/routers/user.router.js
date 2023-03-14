d094831464664c33ae32c1b4c4dc3c79df2dafcc
Julia Dannerth <jdannerth@gmail.com>
2023-03-14 16:32:53 -0400

    created jwt for authentication user

--- src/routers/user.router.js
+++ src/routers/user.router.js
@@ -4,7 +4,9 @@ const router = express.Router();
 
 const{insertUser, getUserByEmail} = require("../model/user/User.model");
 const{hashPassword, comparePassword} = require("../helpers/bcrypt.helper");
+const { createAccessJWT, createRefreshJWT} = require("../helpers/jwt.helper")
 const {json} = require("body-parser");
+const { createAccessJWT, createRefreshJWT } = require("../helpers/jwt.helper.js");
 
 
 router.all("/", (req, res, next) => {
@@ -47,20 +49,40 @@ router.post("/login", async (req, res)=> {
 
     const{email, password} = req.body;
 
+
+    //get email and password from request
+    //if email OR password are not there, invalid submission
     if(!email || !password){
        return res.json({status:"error", message:"Invalid Form Submission"});
     }
 
+    //get user from email address provided
      const user = await getUserByEmail(email);
      
+     //if there is an email address, then there is a password inside the email
      const passFromDb = user && user._id ? user.password : null;
 
+     //if we don't haven a password in the database, invalid 
      if(!passFromDb)
      return res.json({status:"error", message:"Invalid email or password"});
 
-     const result = await comparePassword(password, passFromDb)
-    
-    res.json({status:"success", message:"Login Successful!"});
-})
+     //if there is a password, compare the password we have with the hashed password in the database
+     const result = await comparePassword(password, passFromDb);
+
+     //if the passwords don't match, invalid
+     if(!result){
+        res.json({status:"error", message:"Invalid email or password"});
+     }
+    //if the password matches, then create the AccesssJWT and RefreshJWT
+     const accessJWT = await createAccessJWT(user.email);
+     const refreshJWT = await createRefreshJWT(user.email);
+
+     res.json({
+        status:"success",
+        message:"Login Successful!",
+        accessJWT,
+        refreshJWT,
+    });
+});
 
 module.exports = router;
